2b9efc34b8b3c5164b2be0b070040ed5e8beff6a
cristianzaninovic <czaninovic@gmail.com>
2023-08-04 17:09:44 -0400

    little change for render

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,7 @@
 //= link_tree ../images
-//= link_tree ../builds
 //= link_directory ../stylesheets
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link bootstrap.js
 //= link popper.js
-//= link_tree ../builds
+//= link_tree ../builds .css
