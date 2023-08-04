df1044ca0b5d65e367c81a35062c7ec784f160ce
cristianzaninovic <czaninovic@gmail.com>
2023-08-04 16:34:18 -0400

    little posible fix in render to build assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,8 @@
 //= link_tree ../images
-//= link_tree ../builds .css
+//= link_tree ../builds
 //= link_directory ../stylesheets
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link bootstrap.js
 //= link popper.js
-//= link_tree ../fonts
-
 //= link_tree ../builds
